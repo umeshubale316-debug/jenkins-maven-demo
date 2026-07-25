63b53517971468e867daf386667c5557b90c40ce
umeshubale316-debug <umeshubale316@email.com>
2026-07-25 14:34:34 +0530

    first commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,17 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("          MY PORTFOLIO");
+        System.out.println("=================================");
+        System.out.println("Name       : Umesh Ubale");
+        System.out.println("Role       : AWS DevOps Engineer");
+        System.out.println("Education  : B.Sc (Computer Science)");
+        System.out.println("Skills     : AWS, Docker, Jenkins, Git, Linux, Maven");
+        System.out.println("Languages  : Java, Python");
+        System.out.println("=================================");
+        System.out.println("Thank You!");
+        System.out.println("=================================");
+    }
+}
